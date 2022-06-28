80c3e35cb2fa129d6e90409aceff72790a67fc6c
Giovanni Contino <gcontino@qubicaamf.com>
2022-06-28 14:59:42 +0200

    fixed base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
+import path from "path";
 import vue from '@vitejs/plugin-vue'
-
-import path = require('path')
 
 export default defineConfig({
   plugins: [vue()],
+  base: "/anubis-avatar-creator",
   resolve: {
     alias: [
       {
