8c31e07de893ae74099b9a0ee7c213a7403281e9
Giovanni Contino <gcontino@qubicaamf.com>
2022-06-29 15:58:50 +0200

    fixed conf

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,18 @@ import path from "path";
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()],
-  base: "/anubis-avatar-creator",
-  resolve: {
-    alias: [
-      {
-        find: /^@\/(.+)/,
-        replacement: path.resolve(__dirname, 'src') + '/$1',
-      },
-    ],
-  },
+	plugins: [vue()],
+	base: "/anubis-avatar-creator/",
+	server: {
+		https: true,
+		open: true
+	},
+	resolve: {
+		alias: [
+		{
+			find: /^@\/(.+)/,
+			replacement: path.resolve(__dirname, 'src') + '/$1',
+		},
+		],
+	},
 })
